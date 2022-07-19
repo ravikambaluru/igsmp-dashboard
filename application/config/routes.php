81d227014a9d7818450cc4c8bb25f8d774a02e0f
Raviteja <kambaluri.raviteja@convey.in>
2022-07-19 20:51:53 +0530

    modified

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'master';
+$route['default_controller'] = 'auth/login';
 $route['404_override'] = 'errors/e404';
 $route['translate_uri_dashes'] = FALSE;
 
